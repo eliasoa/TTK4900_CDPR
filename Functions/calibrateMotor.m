7cb279ed72fbaca365a402caf34b7e4721eec947
magag <magag@10.22.65.233>
2024-03-12 14:35:52 +0100

    Added main calibration script.

--- Functions/calibrateMotor.m
+++ Functions/calibrateMotor.m
@@ -2,19 +2,30 @@ function calibrateMotor(device, ODriveEnums)
 
 % Erase existing config and reboot
 eraseConfig(device)
+pause(1)
 
 % Add new config/motor calibration
 calibrationConfig(device)
 
 % Save new config and reboot
 saveConfig(device)
+pause(1)
 
 % Calibrate motor
 writeParameter(device, "axis0.requested_state", ODriveEnums.AxisState.AXIS_STATE_MOTOR_CALIBRATION)
-pause(???)  % Wait for end of motor beep
+pause(5)  % Wait for end of motor beep
 
 % Save new config and reboot
 saveConfig(device)
 
-% Calibrate Encoder Offset
+% Calibrate Encoder Offset (?)
+writeParameter(device, "axis0.requested_state", ODriveEnums.AxisState.AXIS_STATE_ENCODER_OFFSET_CALIBRATION)
+pause()
+
+% Save new config and reboot
+saveConfig(device)
+
+% Can use "axis0.requested_state" == 
+% ODriveEnums.AxisState.AXIS_STATE_FULL_CALIBRATION_SEQUENCE, this runs
+% both motor and encoder offset calibration
 
