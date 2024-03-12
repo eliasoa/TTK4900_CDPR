0c94b86e001c1c9b600000280449fbc4a9338228
eliasoa <eliasoa@NTNU16266.win.ntnu.no>
2024-03-12 14:37:57 +0100

    blergh

--- Functions/motorPosTest.m
+++ Functions/motorPosTest.m
@@ -4,8 +4,13 @@ userInput = input('Enter position setpoint  [-1, 1]: ', 's'); % 's' for string i
 x_target = str2double(userInput); % Convert to number
 disp(['You entered the setpoint: ', num2str(x_target)]);
 
+
 errorEncountered = false;
 fieldNames = fieldnames(ODriveStruct);
+
+h = 0.01;
+e_int = 0;
+
 motorPos = [0;0];
 
 
@@ -37,24 +42,33 @@ while true
     end
     x_r = motorPos(1);
     x_l = motorPos(2);
-    x_current = (x_r - x_l) / 2;
+    x_current = (x_l - x_r) / 2;
     e = x_target - x_current % Positive error, go right
 
 
     %% Tension logic
     % Given values
-    t_ref = -0.2;
+    t_ref = -0.3;
     t_min = -0.1;  % Less tension
-    t_max = -0.3;  % More tension
+    t_max = -0.5;  % More tension
 
-    Kp = .5; % Tunable gain
-
+    Kp = 1.5; % Tunable gain
+    Ki = 1;
     % Calculate tension adjustment based on error
-    t = e * Kp;
+    e_int = e_int + e * h;
+    
+    e_tol = 0.1; 
+    if e_int >= e_tol 
+        e_int = e_tol;
+    elseif e_int <=  -e_tol
+        e_int = -e_tol;
+    end
+
+    t = e * Kp - e_int * Ki
 
     % Calculate tension for each motor, ensuring it stays within limits
-    T_left = max(min(t_ref - t, t_min), t_max);
-    T_right = max(min(t_ref + t, t_min), t_max);
+    T_right = max(min(t_ref - t, t_min), t_max);
+    T_left = max(min(t_ref + t, t_min), t_max);
 
     % Tension vector to be sent to motors
     T = [T_right; T_left]
@@ -64,10 +78,10 @@ while true
         setMotorTorque(T(k), ODrive);
     end
     
-    if abs(e) < 0.05
-        pause(3);
-        break;
-    end
+    % if abs(e) < 0.01
+    %     pause(3);
+    %     break;
+    % end
 
 
 end
