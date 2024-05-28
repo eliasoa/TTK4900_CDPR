8ddac1a489d9bb2b678ded606b799b7b47fdf4fc
eliasoa <eliasoa@NTNU16266.win.ntnu.no>
2024-05-28 20:32:48 +0200

    added some suff

--- Plotting.m
+++ Plotting.m
@@ -1,19 +1,30 @@
-
-Data = load("PlotData/PoseData.mat");
+clear all
+Data = load("C:\Users\eliasoa\OneDrive - NTNU\Documents - Modellering, regulatordesign og simulering av kabeldrevet robot\General\TTK4900 Masteroppgave\Rapport\Tester og fremstillig av plots\MATLAB RUN\PlotData.mat");
 
 q           = Data.q_log;
 qd          = Data.qd_log;
 e           = Data.e_log;  
 e_int       = Data.e_int_log;  
 f           = Data.f_log;
-T_friction  = Data.T_friction_log;
 timeVec     = Data.timeVec;
-vel         = Data.vel_log;
-fvel        = Data.fvel_log;
-f_uncon     = Data.f_uncon_log;
-f_friction  = Data.f_friction_log;
-f_friction_signed = Data.f_friction_signed_log;
-w_friction  = Data.w_friction_log;
+
+
+%% Plot
+figure
+hold on
+plot(q(1,:),q(2,:));
+
+
+%%
+
+% T_friction  = Data.T_friction_log;
+
+% vel         = Data.vel_log;
+% fvel        = Data.fvel_log;
+% f_uncon     = Data.f_uncon_log;
+% f_friction  = Data.f_friction_log;
+% f_friction_signed = Data.f_friction_signed_log;
+% w_friction  = Data.w_friction_log;
 w_c         = Data.w_c_log;
 
 iterations    = length(q);
@@ -88,178 +99,178 @@ close all;
 % xlabel("Iteration Number", "Interpreter","latex")
 % ylabel("Orientation (deg)", "Interpreter","latex")
 
-figure(4)
-plot(q(1,:), q(2,:), qd(1,:), qd(2,:))
-legend("Pose", "Desired Pose")
-xlim([-0.7, 0.7])
-ylim([-0.5, 0.5])
-grid on;
-
-figure(5)
-subplot(3,1,1)
-plot(timeVec , q(1,:), timeVec ,qd(1,:));
-title("Plot of x", "Interpreter", "latex")
-legend("x", "$x_d$","Interpreter", "latex")
-xlabel("Time (s)", "Interpreter","latex")
-ylabel("Position (m)", "Interpreter","latex")
-
-subplot(3,1,2)
-plot(timeVec , q(2,:), timeVec ,qd(2,:));
-title("Plot of y", "Interpreter", "latex")
-legend("y", "$y_d$","Interpreter", "latex")
-xlabel("Time (s)", "Interpreter","latex")
-ylabel("Position (m)", "Interpreter","latex")
-
-subplot(3,1,3)
-plot(timeVec , rad2deg(q(3,:)), timeVec, rad2deg(qd(3,:)) );
-title("Plot of $\phi$", "Interpreter", "latex")
-legend("$\phi$", "$\phi _d$","Interpreter", "latex")
-xlabel("Time (s)", "Interpreter","latex")
-ylabel("Angle (deg)", "Interpreter","latex")
-
-% figure(6)
-% subplot(3,1,1)
-% plot(timeVec, e(1,:));
-% title("Error in x", "Interpreter", "latex")
-% legend("$e_x$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Position (m)", "Interpreter","latex")
-% 
-% subplot(3,1,2)
-% plot(timeVec, e(2,:));
-% title("Error in y", "Interpreter", "latex")
-% legend("$e_y$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Position (m)", "Interpreter","latex")
-% 
-% subplot(3,1,3)
-% plot(timeVec , rad2deg(e(3,:)));
-% title("Error in $\phi$", "Interpreter", "latex")
-% legend("$e_{\phi}$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Orientation (deg)", "Interpreter","latex")
-% 
-% 
-% figure(7)
-% subplot(3,1,1)
-% plot(timeVec, e_int(1,:));
-% title("Error in x", "Interpreter", "latex")
-% legend("$e_x$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Position (m)", "Interpreter","latex")
-% 
-% subplot(3,1,2)
-% plot(timeVec , e_int(2,:));
-% title("Error in y", "Interpreter", "latex")
-% legend("$e_y$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Position (m)", "Interpreter","latex")
-% 
-% subplot(3,1,3)
-% plot(timeVec ,(e_int(3,:)));
-% title("Error in $\phi$", "Interpreter", "latex")
-% legend("$e_{\phi}$","interpreter", "latex")
-% xlabel("Time (s)", "Interpreter","latex")
-% ylabel("Orientation (deg)", "Interpreter","latex")
-% 
-%  
-% figure(8)
-% subplot(2,1,1)
+% figure(4)
 % plot(q(1,:), q(2,:), qd(1,:), qd(2,:))
 % legend("Pose", "Desired Pose")
 % xlim([-0.7, 0.7])
 % ylim([-0.5, 0.5])
 % grid on;
-
-figure(9)
-subplot(2,1,1)
-hold on
-plot(timeVec,f);
-legend('$f_1$','$f_2$','$f_3$','$f_4$',"Interpreter", "latex")
-subplot(2,1,2)
-plot(timeVec,T_friction)
-legend('$f_1$','$f_2$','$f_3$','$f_4$',"Interpreter", "latex")
-
-figure(10)
-title("Friction vs Velocity ")
-subplot(2,2,3)
-plot(fvel(1,:), T_friction(1,:), 'o')
-title("ODrive0")
-xlabel("Velocity","Interpreter","latex")
-ylabel("Force", "Interpreter","latex")
-
-subplot(2,2,1)
-plot(fvel(2,:), T_friction(2,:),'o')
-title("ODrive1")
-xlabel("Velocity","Interpreter","latex")
-ylabel("Force", "Interpreter","latex")
-
-subplot(2,2,2)
-plot(fvel(3,:), T_friction(3,:),'o')
-title("ODrive2")
-xlabel("Velocity","Interpreter","latex")
-ylabel("Force", "Interpreter","latex")
-
-subplot(2,2,4)
-plot(fvel(4,:), T_friction(4,:),'o')
-title("ODrive3")
-xlabel("Velocity","Interpreter","latex")
-ylabel("Force", "Interpreter","latex")
-
-figure(11)
-subplot(2,2,3)
-plot(timeVec, vel(1,:)); hold on;
-plot(timeVec, fvel(1,:)); hold off;
-title("ODrive0", "Interpreter","latex")
-xlabel("Time","Interpreter","latex")
-ylabel("Velocity", "Interpreter","latex")
-legend('Unfiltered','Filtered')
-
-subplot(2,2,1)
-plot(timeVec, vel(2,:)); hold on;
-plot(timeVec, fvel(2,:)); hold off;
-title("ODrive1", "Interpreter","latex")
-xlabel("Time","Interpreter","latex")
-ylabel("Velocity", "Interpreter","latex")
-legend('Unfiltered','Filtered')
-
-subplot(2,2,2)
-plot(timeVec, vel(3,:)); hold on;
-plot(timeVec, fvel(3,:)); hold off;
-title("ODrive2", "Interpreter","latex")
-xlabel("Time","Interpreter","latex")
-ylabel("Velocity", "Interpreter","latex")
-legend('Unfiltered','Filtered')
-
-subplot(2,2,4)
-plot(timeVec, vel(4,:)); hold on;
-plot(timeVec, fvel(4,:)); hold off;
-title("ODrive3", "Interpreter","latex")
-xlabel("Time","Interpreter","latex")
-ylabel("Velocity", "Interpreter","latex")
-legend('Unfiltered','Filtered')
-%%
-figure(12)
-plot(timeVec,w_friction(1,:)+w_c(1,:))
-legend("$w_{fric,x}$","$w_y$","$\tau_z$","Interpreter", "latex")
-
-figure(13)
-plot(timeVec, w_c(1,:))
-legend("$w_x$","$w_y$","$\tau_z$","Interpreter", "latex")
-%%
-cable = 1;
-figure(14)
-hold on
-plot(tIter,f(cable,:))
-plot(tIter,f_friction(cable,:))
-% plot(tIter,f_friction_signed(cable,:),'--')
-hold off
-
-cable = 2;
-figure(15)
-hold on
-plot(timeVec,f(cable,:))
-plot(timeVec,f_friction(cable,:))
-% plot(timeVec,f_friction_signed(cable,:),'--')
+% 
+% figure(5)
+% subplot(3,1,1)
+% plot(timeVec , q(1,:), timeVec ,qd(1,:));
+% title("Plot of x", "Interpreter", "latex")
+% legend("x", "$x_d$","Interpreter", "latex")
+% xlabel("Time (s)", "Interpreter","latex")
+% ylabel("Position (m)", "Interpreter","latex")
+% 
+% subplot(3,1,2)
+% plot(timeVec , q(2,:), timeVec ,qd(2,:));
+% title("Plot of y", "Interpreter", "latex")
+% legend("y", "$y_d$","Interpreter", "latex")
+% xlabel("Time (s)", "Interpreter","latex")
+% ylabel("Position (m)", "Interpreter","latex")
+% 
+% subplot(3,1,3)
+% plot(timeVec , rad2deg(q(3,:)), timeVec, rad2deg(qd(3,:)) );
+% title("Plot of $\phi$", "Interpreter", "latex")
+% legend("$\phi$", "$\phi _d$","Interpreter", "latex")
+% xlabel("Time (s)", "Interpreter","latex")
+% ylabel("Angle (deg)", "Interpreter","latex")
+% 
+% % figure(6)
+% % subplot(3,1,1)
+% % plot(timeVec, e(1,:));
+% % title("Error in x", "Interpreter", "latex")
+% % legend("$e_x$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Position (m)", "Interpreter","latex")
+% % 
+% % subplot(3,1,2)
+% % plot(timeVec, e(2,:));
+% % title("Error in y", "Interpreter", "latex")
+% % legend("$e_y$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Position (m)", "Interpreter","latex")
+% % 
+% % subplot(3,1,3)
+% % plot(timeVec , rad2deg(e(3,:)));
+% % title("Error in $\phi$", "Interpreter", "latex")
+% % legend("$e_{\phi}$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Orientation (deg)", "Interpreter","latex")
+% % 
+% % 
+% % figure(7)
+% % subplot(3,1,1)
+% % plot(timeVec, e_int(1,:));
+% % title("Error in x", "Interpreter", "latex")
+% % legend("$e_x$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Position (m)", "Interpreter","latex")
+% % 
+% % subplot(3,1,2)
+% % plot(timeVec , e_int(2,:));
+% % title("Error in y", "Interpreter", "latex")
+% % legend("$e_y$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Position (m)", "Interpreter","latex")
+% % 
+% % subplot(3,1,3)
+% % plot(timeVec ,(e_int(3,:)));
+% % title("Error in $\phi$", "Interpreter", "latex")
+% % legend("$e_{\phi}$","interpreter", "latex")
+% % xlabel("Time (s)", "Interpreter","latex")
+% % ylabel("Orientation (deg)", "Interpreter","latex")
+% % 
+% %  
+% % figure(8)
+% % subplot(2,1,1)
+% % plot(q(1,:), q(2,:), qd(1,:), qd(2,:))
+% % legend("Pose", "Desired Pose")
+% % xlim([-0.7, 0.7])
+% % ylim([-0.5, 0.5])
+% % grid on;
+% 
+% figure(9)
+% subplot(2,1,1)
+% hold on
+% plot(timeVec,f);
+% legend('$f_1$','$f_2$','$f_3$','$f_4$',"Interpreter", "latex")
+% subplot(2,1,2)
+% plot(timeVec,T_friction)
+% legend('$f_1$','$f_2$','$f_3$','$f_4$',"Interpreter", "latex")
+% 
+% figure(10)
+% title("Friction vs Velocity ")
+% subplot(2,2,3)
+% plot(fvel(1,:), T_friction(1,:), 'o')
+% title("ODrive0")
+% xlabel("Velocity","Interpreter","latex")
+% ylabel("Force", "Interpreter","latex")
+% 
+% subplot(2,2,1)
+% plot(fvel(2,:), T_friction(2,:),'o')
+% title("ODrive1")
+% xlabel("Velocity","Interpreter","latex")
+% ylabel("Force", "Interpreter","latex")
+% 
+% subplot(2,2,2)
+% plot(fvel(3,:), T_friction(3,:),'o')
+% title("ODrive2")
+% xlabel("Velocity","Interpreter","latex")
+% ylabel("Force", "Interpreter","latex")
+% 
+% subplot(2,2,4)
+% plot(fvel(4,:), T_friction(4,:),'o')
+% title("ODrive3")
+% xlabel("Velocity","Interpreter","latex")
+% ylabel("Force", "Interpreter","latex")
+% 
+% figure(11)
+% subplot(2,2,3)
+% plot(timeVec, vel(1,:)); hold on;
+% plot(timeVec, fvel(1,:)); hold off;
+% title("ODrive0", "Interpreter","latex")
+% xlabel("Time","Interpreter","latex")
+% ylabel("Velocity", "Interpreter","latex")
+% legend('Unfiltered','Filtered')
+% 
+% subplot(2,2,1)
+% plot(timeVec, vel(2,:)); hold on;
+% plot(timeVec, fvel(2,:)); hold off;
+% title("ODrive1", "Interpreter","latex")
+% xlabel("Time","Interpreter","latex")
+% ylabel("Velocity", "Interpreter","latex")
+% legend('Unfiltered','Filtered')
+% 
+% subplot(2,2,2)
+% plot(timeVec, vel(3,:)); hold on;
+% plot(timeVec, fvel(3,:)); hold off;
+% title("ODrive2", "Interpreter","latex")
+% xlabel("Time","Interpreter","latex")
+% ylabel("Velocity", "Interpreter","latex")
+% legend('Unfiltered','Filtered')
+% 
+% subplot(2,2,4)
+% plot(timeVec, vel(4,:)); hold on;
+% plot(timeVec, fvel(4,:)); hold off;
+% title("ODrive3", "Interpreter","latex")
+% xlabel("Time","Interpreter","latex")
+% ylabel("Velocity", "Interpreter","latex")
+% legend('Unfiltered','Filtered')
+% %%
+% figure(12)
+% plot(timeVec,w_friction(1,:)+w_c(1,:))
+% legend("$w_{fric,x}$","$w_y$","$\tau_z$","Interpreter", "latex")
+% 
+% figure(13)
+% plot(timeVec, w_c(1,:))
+% legend("$w_x$","$w_y$","$\tau_z$","Interpreter", "latex")
+% %%
+% cable = 1;
+% figure(14)
+% hold on
+% plot(tIter,f(cable,:))
+% plot(tIter,f_friction(cable,:))
+% % plot(tIter,f_friction_signed(cable,:),'--')
+% hold off
+% 
+% cable = 2;
+% figure(15)
+% hold on
+% plot(timeVec,f(cable,:))
+% plot(timeVec,f_friction(cable,:))
+% % plot(timeVec,f_friction_signed(cable,:),'--')
 hold off
 
--- testing.m
+++ testing.m
@@ -4,6 +4,8 @@ R           = CDPR_Params.Gen_Params.SpoolParams.SPOOL_RADIUS;        % Radius o
 P           = CDPR_Params.Gen_Params.SpoolParams.PITCH;
 r_p         = 0.012; % raduis trinse
 a           = CDPR_Params.SGM.FrameAP;                    % Frame Anchor Points
+a           = [-0.7510,   -0.7510,    0.7510,    0.7510;
+               -0.4181,    0.4181,    0.4181,   -0.4181];
 b           = CDPR_Params.SGM.BodyAP.TRAPEZOID;           % Body Anchor Points
 motorsigns  = CDPR_Params.Gen_Params.MOTOR_SIGNS;         % Signs determining positive rotational direction
 p0          = CDPR_Params.Gen_Params.EncoderOffset;
@@ -11,14 +13,14 @@ f_min = 10;
 f_max = 80;
 f_ref = (f_max +f_min)/2;
 
-f_static = [2; 7; 0; 0];
-f_static = [0; 0; 0; 0];
-t_static = [0.056; 0.0504; 0.0498; 0.0354];
-fvel3 = 0;
+% f_static = [2; 7; 0; 0];
+% f_static = [0; 0; 0; 0];
+% t_static = [0.056; 0.0504; 0.0498; 0.0354];
+% fvel3 = 0;
 fvel = zeros(4,1);
 
-x_diff = 56.40783*1e-3;
-y_diff = 371.4759*1e-3;
+% x_diff = 56.40783*1e-3;
+% y_diff = 371.4759*1e-3;
 
 iter = 200;
 count       = 1;
@@ -32,6 +34,7 @@ T_friction  = zeros(4,1);
 % Log vectors
 q_log           = zeros(3,iter);
 qd_log          = zeros(3,iter);
+q_dotf_log      = zeros(3,iter);
 e_log           = zeros(3,iter);
 e_int_log       = zeros(3,iter);
 f_log           = zeros(4,iter);
@@ -39,24 +42,27 @@ T_friction_log  = zeros(4,iter);
 vel_log         = zeros(4,iter);
 fvel_log        = zeros(4,iter);
 
-f_uncon_log     = zeros(4,iter);
-f_friction_log  = zeros(4,iter);
-w_friction_log  = zeros(3,iter);
+% f_uncon_log     = zeros(4,iter);
+% f_friction_log  = zeros(4,iter);
+% w_friction_log  = zeros(3,iter);
 
 w_c_log         = zeros(3,iter);
 
 
 % l0 = [1.2260 1.1833 1.1833 1.2260]';
 l0 = [1.2060 1.1820 1.1820 1.2060]';
+l0 = [1.196549826581494 1.172604084137631 1.173956813721921 1.202012667688769]';
+lde2pe = [0.359809755287430 0.363766765936637 0.361788142425923 0.361788142425923]';
+omega = 2.5;
+Ly = 0.1;
+Lx = 0.1;
 
-omega = 2;
-Ly = 0.15;
-Lx = 0.15;
-
-Kp = diag([200; 175; 5]);
-Ki = diag([0;0;0]);
-Kd = diag([10 10 0]);
+Kp = diag([225; 125; 5]);
+Ki = diag([425;250;0]);
+Kd = diag([15 21 0]);
 e_int   = zeros(3,1);
+alpha = [0.4;0.4;0.3];
+q_dotf = zeros(3,1);
 
 fieldNames = fieldnames(ODriveStruct);
 
@@ -70,57 +76,60 @@ while count <= iter
     l_enc = pos_rad*R.*motorsigns;
     l_q = l0 + l_enc;
     w_d = pos_n*P;
-    l_spool_to_pulley = sqrt(sqrt(w_d.^2+y_diff^2).^2 + x_diff^2);
+    l_spool_to_pulley = sqrt(w_d.^2 + lde2pe.^2);
     l_q_fk = l_q - l_spool_to_pulley;
 
     q_0 = init_fk_estimate(a,b,l_q_fk);
     q = p_forward_kinematics(a,b,l_q_fk,q_0,r_p);
 
     time = toc(s_start);
-    q_d         = [Lx*sin(omega*time);Ly*cos(omega*time)-Ly;0];
-    q_d_dot     = [-Lx*cos(omega*time); Ly*sin(omega*time); 0];
+    q_d         = [Lx*sin(omega*time);Ly*cos(omega*time)-0.1;0];
+    q_d_dot     = [-Lx*omega*cos(omega*time); Ly*omega*sin(omega*time); 0];
 
     % Calculate Structure Matrix
     [~,betar]   = p_inverse_kinematics(a,b,q, r_p);
     A_t         = structure_matrix(a,b,q,r_p,betar);
     A_pseudo    = pinv(A_t');
-    A_t_pinv    = pinv(A_t);
+    % A_t_pinv    = pinv(A_t);
 
     q_dot = -A_pseudo*l_dot;
+    q_dotf = [ alpha(1) * q_dot(1) + (1-alpha(1))*q_dotf(1);
+               alpha(2) * q_dot(2) + (1-alpha(2))*q_dotf(2);
+               alpha(3) * q_dot(3) + (1-alpha(3))*q_dotf(3)];
 
     % Calculate Errors
     e           = q_d - q;
-    e_dot       = q_d_dot - q_dot;
+    e_dot       = q_d_dot - q_dotf;
 
     % Desired wrench (TESTE KONTROLLER)
     w_cP = Kp*e;
     w_cI = Ki*e_int;
     w_cD = Kd*e_dot;
     w_c =  w_cP + w_cI + w_cD;
-    w_c = [15;0;0];
+    % w_c = [15;0;0];
     
-    f_uncon = A_t_pinv*w_c;
+    % f_uncon = A_t_pinv*w_c;
 
     % Force Allocation
     [f,~,flag] = ForceAllocIterativeSlack(A_t',f_min,f_max,f_ref,f_prev,w_c);
     % fvel3 = 0.9*fvel3 + 0.1*vel(3);
-    fvel = 0.6*fvel + 0.4*vel;
-    for i = 1:4
-        % if i==4
-            % T_friction(i) = FrictionModel(i-1,fvel3);
-        % else
-            T_friction(i) = FrictionModel(i-1,fvel(i));
-            T_friction_signed(i) = T_friction(i)*motorsigns(i);
-        % end
-        
-    end
-    f_friction = T_friction./R;
-    f_friction_signed = T_friction_signed./R;
-    w_friction = -A_t*f_friction;
+    % fvel = 0.6*fvel + 0.4*vel;
+    % for i = 1:4
+    %     % if i==4
+    %         % T_friction(i) = FrictionModel(i-1,fvel3);
+    %     % else
+    %         T_friction(i) = FrictionModel(i-1,fvel(i));
+    %         T_friction_signed(i) = T_friction(i)*motorsigns(i);
+    %     % end
+    % 
+    % end
+    % f_friction = T_friction./R;
+    % f_friction_signed = T_friction_signed./R;
+    % w_friction = -A_t*f_friction;
   
-    T_s = ...
-    sign(A_t_pinv*w_c).*(~(ones(4,1)&fix(fvel*10^3)/10^3)...
-    &any(fix(e*10^3)/10^3)).*t_static
+    % T_s = ...
+    % sign(A_t_pinv*w_c).*(~(ones(4,1)&fix(fvel*10^3)/10^3)...
+    % &any(fix(e*10^3)/10^3)).*t_static
     % fix(vel*10^3)/10^3
     % any(fix(e*10^3)/10^3)
     % (ones(4,1)&fix(vel*10^1)/10^1)
@@ -143,6 +152,7 @@ while count <= iter
     % Logging
     q_log(:,count)      = q;
     qd_log(:,count)     = q_d;
+    q_dotf_log(:,count) = q_dotf;
     e_log(:,count)      = e;
     e_int_log(:,count)  = e_int;
     f_log(:,count)      = f;
@@ -150,10 +160,10 @@ while count <= iter
     vel_log(:,count)    = vel;    
     fvel_log(:,count)   = fvel;    
 
-    f_uncon_log(:,count) = f_uncon;
-    f_friction_log(:,count) = f_friction;
-    f_friction_signed_log(:,count) = f_friction_signed;
-    w_friction_log(:, count) = w_friction;
+    % f_uncon_log(:,count) = f_uncon;
+    % f_friction_log(:,count) = f_friction;
+    % f_friction_signed_log(:,count) = f_friction_signed;
+    % w_friction_log(:, count) = w_friction;
     
     w_c_log(:,count) = w_c;
 
@@ -165,23 +175,25 @@ while count <= iter
 end
 
 %% Save logged data
-save("PlotData/PoseData.mat", "q_log","qd_log","e_log","e_int_log","f_log","T_friction_log","f_uncon_log","f_friction_log","f_friction_signed_log", "w_friction_log", "w_c_log", "timeVec", "vel_log", "fvel_log");
+% save("PlotData/PoseData.mat", "q_log","qd_log","e_log","e_int_log","f_log","T_friction_log","f_uncon_log","f_friction_log", "w_friction_log", "w_c_log", "timeVec", "vel_log", "fvel_log");
+save("C:\Users\eliasoa\OneDrive - NTNU\Documents - Modellering, regulatordesign og simulering av kabeldrevet robot\General\TTK4900 Masteroppgave\Rapport\Tester og fremstillig av plots\MATLAB RUN\PlotData.mat", ...
+    "q_log","qd_log","e_log","e_int_log","f_log","timeVec");
 % Plot of total time and Hertz
-% figure(1)
-% subplot(2,1,1)
-% hold on
-% plot(total_time)
-% ylabel({'Seconds'});
-% xlabel({'Iteration number'});
-% title({'Loop time'});
-% hold off
-% subplot(2,1,2)
-% hold on
-% plot(1./total_time)
-% ylabel({'Hz'});
-% xlabel({'Iteration number'});
-% title({'Loop frequency'});
-% hold off
+figure
+subplot(2,1,1)
+hold on
+plot(total_time)
+ylabel({'Seconds'});
+xlabel({'Iteration number'});
+title({'Loop time'});
+hold off
+subplot(2,1,2)
+hold on
+plot(1./total_time)
+ylabel({'Hz'});
+xlabel({'Iteration number'});
+title({'Loop frequency'});
+hold off
 T = [0.2;-0.2;0.2;-0.2];
 for k = 1:length(fieldNames)
     fieldName = fieldNames{k}; % Current field name as a string
